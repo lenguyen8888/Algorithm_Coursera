e78a1bcda035c23c0f145a29c51e38d32c5e515b
Nguyen Le <lenguyen_d@hotmail.com>
2023-03-31 21:58:35 -0700

    Initial implementation for Week0

---
+++ Alg_I_week0/src/RandomWord.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.StdOut;
+
+// Write a program RandomWord.java that reads a sequence of words from standard input and prints
+// one of those words uniformly at random. Do not store the words in an array or list.
+// Instead, use Knuth’s method: when reading the ith word, select it with probability 1/i
+// to be the champion, replacing the previous champion. 
+// After reading all of the words, print the surviving champion.
+import edu.princeton.cs.algs4.*;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        String champion = "";
+        int i = 0;
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            i++;
+            if (Math.random() < 1.0/i) {
+                champion = word;
+            }
+        }
+        StdOut.println(champion);
+    }
+    
+}
---
+++ Alg_I_week0/src/build_zip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+javac-algs4 HelloWorld.java
+javac-algs4 HelloGoodbye.java
+javac-algs4 RandomWord.java
+
+java-algs4 HelloWorld
+
+java-algs4 HelloGoodbye Kevin Bob
+java-algs4 HelloGoodbye Alejandra Bahati
+
+java-algs4 RandomWord < ../../debug_data/hello/animals8.txt
+
+jar -cfM hello.zip HelloWorld.java HelloGoodbye.java RandomWord.java
+
